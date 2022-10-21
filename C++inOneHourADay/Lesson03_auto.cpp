1d10f07c330953981ac74ea85af232666a22bc71
ronansena <ronansena@protonmail.com>
2022-10-21 14:38:14 -0300

    Update

---
+++ C++inOneHourADay/Lesson03_auto.cpp
@@ -0,0 +1,23 @@
+
+ #include <iostream>
+
+ using namespace std;
+ int main()
+
+ {
+
+ auto coinFlippedHeads = true;
+
+ auto largeNumber = 2500000000000;
+ 
+ cout  <<  "coinFlippedHeads = " << coinFlippedHeads << ", ";
+
+ cout  << "sizeof(coinFlippedHeads) = " << sizeof(coinFlippedHeads);
+
+ cout  << endl << "largeNumber = " << largeNumber << ", ";
+
+ cout << "sizeof(largeNumber) = " << sizeof(largeNumber) << endl;
+
+ return 0;
+
+ }
