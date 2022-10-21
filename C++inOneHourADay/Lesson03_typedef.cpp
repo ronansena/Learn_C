4bf225def4c716003e2e9593572f615d8a7f542f
ronansena <ronansena@protonmail.com>
2022-10-21 14:45:55 -0300

    Update

---
+++ C++inOneHourADay/Lesson03_typedef.cpp
@@ -0,0 +1,20 @@
+
+ #include <iostream>
+
+ using namespace std;
+ int main()
+
+ {
+
+ typedef bool TRUE_OR_FALSE_TYPE;
+ 
+ TRUE_OR_FALSE_TYPE  coinFlippedHeads = true;
+ 
+ cout  <<  "coinFlippedHeads = " << coinFlippedHeads << ", ";
+
+ cout  << "sizeof(coinFlippedHeads) = " << sizeof(coinFlippedHeads);
+
+
+ return 0;
+
+ }
